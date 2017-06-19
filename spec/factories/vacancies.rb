d0ba4eb3344ee14b225d67ef0a22a23ff425d8a8
RuslanShmid <ruslan.shmid@lasoft.org>
2017-06-19 15:25:55 +0300

    refactor tests

--- spec/factories/vacancies.rb
+++ spec/factories/vacancies.rb
@@ -3,11 +3,12 @@ FactoryGirl.define do
     title { Faker::Job.field }
     category { Faker::Name.last_name }
     city { Faker::Address.city }
+    email { Faker::Internet.email }
     active_to_date { Faker::Date.between(2.days.ago, Date.today) }
-    job_type nil
-    description { Faker::Lorem.sentences(1) }
+    job_type { Faker::Job.title }
+    description { Faker::Lorem.sentence(3, true, 4) }
     company { Faker::Company.name }
-    price_per_hour nil
+    price_per_hour { Faker::Number.between(5, 30) }
     website nil
   end
 end
