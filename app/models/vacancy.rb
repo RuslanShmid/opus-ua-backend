f5a56d5e2e199a15ab99b9839d2dbac026639570
RuslanShmid <ruslan.shmid@lasoft.org>
2017-06-19 15:53:01 +0300

    deleted title regex

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -3,8 +3,7 @@ class Vacancy < ApplicationRecord
 
   validates :title,
             presence: true,
-            length: { in: 2..30 },
-            format: { with: /\A^([^0-9]*)$\z/ }
+            length: { in: 2..30 }
 
   validates :email,
             presence: true,
